15ac3a7a1d879c4f7c06ea3249dce33bee24d6ff
Michael Partin <unmykx@gmmail.com>
2020-01-22 20:12:36 -0800

    fixed wrong reference to table

--- models/burger.js
+++ models/burger.js
@@ -2,17 +2,17 @@ const orm = require('../config/orm.js');
 
 const burger = {
     selectAll: function(cb) {
-        orm.selectAll("burger_name", function(res) {
+        orm.selectAll("burgers", function(res) {
         cb(res);
         });
     },
     insertOne: function(cols, vals, cb) {
-        orm.create("burger_name", cols, vals, function(res) {
+        orm.insertOne("burgers", cols, vals, function(res) {
         cb(res);
         });
     },
     update: function(objColVals, condition, cb) {
-        orm.update("burger_name", objColVals, condition, function(res) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
         cb(res);
         });
     }
