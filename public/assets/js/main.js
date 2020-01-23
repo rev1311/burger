43e57f5822c72e9d70ae5dc183d1ac40b13c0fb3
Michael Partin <unmykx@gmmail.com>
2020-01-22 22:00:55 -0800

    switching api names to find where it isnt pathing correctly

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,7 +3,7 @@ $(function() {
       event.preventDefault();
       let newBurger = { name: $("#burger").val() };
   
-      $.ajax(`/api/burgers`, {
+      $.ajax(`/api/burger`, {
         type: "POST",
         data: newBurger
       }).then(function() {
@@ -14,7 +14,7 @@ $(function() {
     $(".eatem").on("click", function(event) {
       event.preventDefault();
       let id = event.target.value;
-      $.ajax(`/api/burgers/${id}`, {
+      $.ajax(`/api/burger/${id}`, {
         type: "PUT"
       }).then(function() {
         location.reload();
