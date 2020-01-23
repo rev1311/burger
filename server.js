a7656b16fb7b2a3a91f28e2519e54be34e0921d6
Michael Partin <unmykx@gmmail.com>
2020-01-22 18:07:52 -0800

    added express connections

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 var express = require('express');
+
+var app = express();
+var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var apiRoutes = require("./controllers/burgers_controller.js");
+
+app.use(apiRoutes);
+
+app.listen(PORT, function() {
+  console.log(`connected on port ${PORT}`);
+});
