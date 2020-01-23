cdf116e88ed57cd756036f403d1d665e7d6d35b2
Michael Partin <unmykx@gmmail.com>
2020-01-22 21:04:36 -0800

    changed button targeting

--- public/js/main.js
+++ public/js/main.js
@@ -2,22 +2,19 @@ $(function() {
     $(".create-form").on("submit", function(event) {
       event.preventDefault();
       let newBurger = { name: $("#burger").val() };
-      console.log(newBurger);
   
-      $.ajax("/api/burgers", {
+      $.ajax(`/api/burgers`, {
         type: "POST",
         data: newBurger
       }).then(function() {
-        console.log("new burger added");
         location.reload();
       });
     });
   
-    $(".devourerer").on("click", function(event) {
+    $(".eatem").on("click", function(event) {
       event.preventDefault();
       let id = event.target.value;
-      console.log(event);
-      $.ajax("/api/burgers/" + id, {
+      $.ajax(`/api/burgers/${id}`, {
         type: "PUT"
       }).then(function() {
         location.reload();
