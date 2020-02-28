71fa467993b1d8d050f0fc6d51320c31660245fc
Michael Partin <unmykx@gmail.com>
2020-02-28 13:55:09 -0800

    trying to fix broken routes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,9 +1,9 @@
 $(function() {
     $(".create-form").on("submit", function(event) {
       event.preventDefault();
-      let newBurger = { name: $("#burger").val(), devoured: true };
+      let newBurger = { name: $("#burger").val(), devoured: false };
       console.log(newBurger);
-      $.ajax(`/api/burger`, {
+      $.ajax('/api/burger', {
         type: "POST",
         data: newBurger
       }).then(function() {
@@ -14,7 +14,7 @@ $(function() {
     $(".eatem").on("click", function(event) {
       event.preventDefault();
       let id = event.target.value;
-      $.ajax(`/api/burger/${id}`, {
+      $.ajax('/api/burger/' + id, {
         type: "PUT"
       }).then(function() {
         location.reload();
