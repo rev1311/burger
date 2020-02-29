d8abfda14de680ad4e4f35e00c2443563f1b2f81
Michael Partin <unmykx@gmail.com>
2020-02-28 18:52:54 -0800

    changed boolean to int for test

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
+    devoured INT DEFAULT 0,
     PRIMARY KEY(id)
 );
 
