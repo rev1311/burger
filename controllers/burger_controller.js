699556255b21e6aa914802fc592d4b047592d2ba
Michael Partin <unmykx@gmail.com>
2020-02-28 21:09:55 -0800

    fixed req,res,result which was causing issues

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -11,8 +11,8 @@ router.get("/", function(req, res) {
     });
 });
 
-router.post("/api/burger", function(req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(req, res) {
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
         res.json({ id: result.insertId });
     });
 }); 
@@ -20,8 +20,8 @@ router.post("/api/burger", function(req, res) {
 router.put("/api/burgers/:id", function(req, res) {
     var condition = req.params.id;
 
-    burger.updateOne({ devoured: req.body.devoured }, condition, function(res) {
-        if (res.changedRows === 0) {
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+        if (result.changedRows === 0) {
         return res.status(404).end();
         } res.status(200).end();
     });
