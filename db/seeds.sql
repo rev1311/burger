c76718c7f06f461fd4a3614e02a89bbb66f8f201
Michael Partin <unmykx@gmail.com>
2020-02-28 16:18:38 -0800

    added more seeds for ttest

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO burgers (burger_name, devoured)
 
-VALUES ("Western Bacon", false), ("Double Double", false), ("Angus", true);
+VALUES ("Western Bacon", false), ("Double Double", false), ("Angus", true),
+ ("plain", false);
