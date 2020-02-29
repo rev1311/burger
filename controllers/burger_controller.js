e8ce4fa96db362ea0639a8aecf6f0bcd927847d1
Michael Partin <unmykx@gmail.com>
2020-02-28 17:22:28 -0800

    trying to get condition varible to pass

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -21,17 +21,25 @@ router.post("/api/burgers", function(req, res) {
 }); 
 
 router.put("/api/burgers/:id", function(req, res) {
-    var condition = req.params.id;
+    var condition = `id=${req.params.id}`;
     console.log("condition", condition);
 
-    burger.updateOne({ devoured: req.body.devoured }, {
-        where: {
-            id: condition
-        }
-    }, function(res) {
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(res) {
         if (res.changedRows === 0) {
         return res.status(404).end();
         } res.status(200).end();
+    });
+});
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = `id=${req.params.id}`;
+    console.log("condition", condition);
+
+    burger.deleteOne(condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
     });
 });
 
