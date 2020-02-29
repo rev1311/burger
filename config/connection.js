19b992bc281474673847c9ff7181119922a6941a
Michael Partin <unmykx@gmail.com>
2020-02-28 16:16:55 -0800

    created variable route for server port

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,9 @@
 const mysql = require('mysql');
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -7,8 +11,7 @@ var connection = mysql.createConnection({
     password: "pass",
     database: "burgers_db"
     });
-
-// var connection = mysql.createConnection(process.env.JAWSDB_URL)
+};
   
   connection.connect(function(err) {
     if (err) {
